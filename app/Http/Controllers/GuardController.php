8e4d926d777a5238ab9e9ff5139db93a3518000d
deli <yasta55688@gmail.com>
2021-04-16 21:14:21 +0800

    建立 auth 登入 範本

---
+++ app/Http/Controllers/GuardController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class GuardController extends Controller
+{
+    //會員註冊
+    public function register(Request $request)
+    {
+
+        // dd($reqeust->all());
+        $this->validate($request, [
+            'account' => 'required|unique:users,account|max:255',
+            'password' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'name' => 'required',
+        ]);
+
+        $userPayload = $request->only('account', 'password', 'email', 'name');
+        $userPayload['password'] = \Hash::make($userPayload['password']);
+        User::create($userPayload);
+        return [
+            'status' => true,
+            'message' => '建立成功',
+        ];
+
+    }
+
+    //會員登入
+    public function login(Request $request)
+    {
+        $payload = $request->only('email', 'account', 'password');
+        $result = auth()->attempt($payload);
+        if (auth()->attempt($payload)) {
+            return [
+                'status' => true,
+                'message' => '登入成功',
+            ];
+        } else {
+            return [
+                'status' => false,
+                'message' => '登入失敗',
+            ];
+        }
+    }
+
+    //顯示當前登入會員
+    public function show()
+    {
+        return auth()->user();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//資料表關聯 attach join
 Route::prefix('attach-example')->group(function () {
     Route::get('user/{user}', 'MigrationController@user');
     Route::post('comment/{post}', 'MigrationController@createComment');
     Route::post('post/{user}', 'MigrationController@createPost');
+});
+
+//登入認證 auth
+Route::prefix('auth-example')->group(function () {
+    Route::post('register', 'GuardController@register');
+    Route::post('login', 'GuardController@login');
+    Route::get('show', 'GuardController@show');
 });
